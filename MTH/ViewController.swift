bf5fa3e51469d6ba161c5e91c4226f4b79bb4af9
Rodion Bychkov <rodion.bychkov@gmail.com>
2021-03-27 19:39:18 +0300

    Initial Commit

---
+++ MTH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MTH
+//
+//  Created by Rodion on 27.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
